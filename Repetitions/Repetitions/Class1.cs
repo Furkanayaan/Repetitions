66a6a6d2658db871e9be485b5ff85d62f0063064
Furkanayaan <ayanf16@itu.edu.tr>
2022-09-12 17:07:00 +0300

    Repetitions

---
+++ Repetitions/Repetitions/Class1.cs
@@ -0,0 +1,32 @@
+﻿using  System;
+namespace Repetitions
+{
+    public class Class1
+    {
+        public static void Main()
+        { 
+            Console.WriteLine("Bir DNA dizisi girin");
+           string DNA = Console.ReadLine();
+           
+           int n = DNA.Length;
+           int cur = 1;
+           int best = 1;
+           
+           for (int i = 1; i < n; i++)
+           {
+               if (DNA[i] == DNA[i-1]) cur++;
+               else
+               {
+                   cur = 1;
+               }
+
+               best = Math.Max(best, cur);
+               
+
+
+           }
+           Console.Write(best);
+           
+        }
+    }
+}
